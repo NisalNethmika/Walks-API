00edc47150587e13e740848876b9221e632d1fec
NisalNethmika <nisalnethmikadezoysa@gmail.com>
2026-04-21 00:39:37 +0530

    added global exception handler middleware

---
+++ WebApp1/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,36 @@
+﻿namespace WebApp1.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+
+        private readonly ILogger<ExceptionHandlerMiddleware> logger;
+        private readonly RequestDelegate next;
+
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger, RequestDelegate next)
+        {
+            this.logger = logger;
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An unhandled exception occurred.");
+                httpContext.Response.StatusCode = 500;
+                httpContext.Response.ContentType = "application/json";
+
+                var error = new
+                {
+                    Error = $"An unhandled exception occurred. Internal server error. {ex.Message}"
+                };
+
+                await httpContext.Response.WriteAsJsonAsync(error);
+            }
+        }
+    }
+}
